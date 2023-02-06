ffe352e7f89ad604a24d9f3a6bd20313d907ed34
Toby <tobyclemson@gmail.com>
2023-02-06 01:01:50 +0000

    Add definition helper method for commit task.

--- lib/rake_git.rb
+++ lib/rake_git.rb
@@ -4,4 +4,7 @@ require 'rake_git/tasks'
 require 'rake_git/version'
 
 module RakeGit
+  def self.define_commit_task(opts = {}, &block)
+    RakeGit::Tasks::Commit.define(opts, &block)
+  end
 end
--- spec/rake_git_spec.rb
+++ spec/rake_git_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe RakeGit do
   it 'has a version number' do
     expect(RakeGit::VERSION).not_to be_nil
   end
+
+  describe 'define_commit_task' do
+    context 'when instantiating RakeGit::Tasks::Commit' do
+      it 'passes the provided block' do
+        opts = {
+          argument_names: [:message]
+        }
+
+        block = lambda do |t, args|
+          t.message = args.message
+        end
+
+        allow(RakeGit::Tasks::Commit).to(receive(:define))
+
+        described_class.define_commit_task(opts, &block)
+
+        expect(RakeGit::Tasks::Commit)
+          .to(have_received(:define) do |passed_opts, &passed_block|
+            expect(passed_opts).to(eq(opts))
+            expect(passed_block).to(eq(block))
+          end)
+      end
+    end
+  end
 end

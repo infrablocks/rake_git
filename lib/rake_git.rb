cf36e469d9b93ed3769ec712023ace08a23ad178
Toby Clemson <tobyclemson@gmail.com>
2024-07-17 17:25:12 +0100

    Fix linting issues.

--- lib/rake_git.rb
+++ lib/rake_git.rb
@@ -8,7 +8,7 @@ require 'rake_git/patches/git_lib'
 Git::Lib.prepend(RakeGit::Patches::GitLib)
 
 module RakeGit
-  def self.define_commit_task(opts = {}, &block)
-    RakeGit::Tasks::Commit.define(opts, &block)
+  def self.define_commit_task(opts = {}, &)
+    RakeGit::Tasks::Commit.define(opts, &)
   end
 end
